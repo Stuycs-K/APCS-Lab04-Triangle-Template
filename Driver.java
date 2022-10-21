5fe44a8eb9dd7cc4b4652a4da60ec2a108ce616b
konstantinnovation <konstans@stuy.edu>
2022-10-21 13:14:06 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver{
+  public static void main(String[]args){
+    //Write your tests here!
+    Point r = new Point(100,100);
+    Point p = new Point(3.0,4.0);
+    Point q = new Point(0.0,0.0);
+    System.out.println(r.distance(p,q)+" should be 4");
+    
+    p = new Point(3.0,4.0);
+    q = new Point(4.0,5.0);
+    System.out.println(p.distanceTo(q)+" should be 1.4142...");
+  }
+}
