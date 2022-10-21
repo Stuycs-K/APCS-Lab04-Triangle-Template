9c5b0fa442cc9c4794bcc476de465a47981d12b5
konstantinnovation <konstans@stuy.edu>
2022-10-21 12:41:18 -0400

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,12 +1,24 @@
 public class Point{
   private double x,y;
   
+  public static boolean closeEnough(double a, double b){
+    //How can you determine if two values are close enough to eachother?
+    return false;
+  }
+  
   /*Return a string in the format: (x, y)
   *where x and y are the values from the Point.
   *e.g.  "(0.0, 1.23)"  or  "(-1.02, 21.45)"
   */
   public String toString(){
     //your code here
+  }
+  
+  /*Return true when the respective x and y values of the two points differ by at most 0.001% each.
+  *To avoid issues: If one is 0.0, the other must be exactly 0.0.
+  */
+  public boolean equals(Point other){
+
   }
   
   public double distanceTo(Point other){
